44edae40d0f079226f9806ad5c8e141f7b9d0f2d
burnworks <6436833+burnworks@users.noreply.github.com>
2025-10-19 20:27:43 +0900

    Update axe-auto-reporter.mjs

--- script/axe-auto-reporter.mjs
+++ script/axe-auto-reporter.mjs
@@ -418,7 +418,6 @@ try {
         '--disable-dev-shm-usage',
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
-        '--no-zygote',
         '--disable-gpu',
         '--memory-pressure-off',
         '--disable-extensions',
